894bba71cc9bf99400cb1c41c127278a28ece8a6
arghyanath <natharghya6294@gmail.com>
2025-08-24 13:51:56 +0000

    fix: fixed auth.ts

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -39,14 +39,6 @@ export const authOptions = {
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async session({ token, session }: { token: JWT, session: Session }) {
-            if (session.user && token.sub) {
-                session.user.id = token.sub;
-            }
-            return session
-        }
-    },
     pages: {
         signIn: "/api/auth/login",
     }
