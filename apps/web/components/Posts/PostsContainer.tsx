582531de78943ff4e893dcf7ec8bd1c96b6134e6
arghyanath <natharghya6294@gmail.com>
2025-08-24 02:28:44 +0000

    fix: fixed fecthing the posts

--- apps/web/components/Posts/PostsContainer.tsx
+++ apps/web/components/Posts/PostsContainer.tsx
@@ -1,38 +1,24 @@
-"use client"
+
+import { getServerSession } from "next-auth";
 import { PostsCard } from "./PostsCard";
-import { authOptions } from "../../lib/auth";
+
 import axios from "axios";
-import { useSession } from "next-auth/react";
-import { useQuery } from "@tanstack/react-query";
-import { Skeleton } from "../ui/skeleton";
-import { SkeletonCard } from "./PostCardSkeleton";
+import { authOptions } from "@/lib/auth";
 
 
-export function PostsContainer() {
-    const session = useSession();
 
-    const { isPending, error, data } = useQuery({
-        queryKey: [`allposts`],
-        queryFn: async () => {
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
-            const posts: Array<Posts> = response.data.posts
-            return posts
-        }
+export async function PostsContainer() {
+    const session = await getServerSession(authOptions);
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
+    const posts: Array<Posts> = response.data.posts
 
 
-    })
-    if (isPending) return <div className="flex flex-col gap-4 items-center w-full">
-        <SkeletonCard />
-        <SkeletonCard />
-        <SkeletonCard />
-    </div>
-    if (!session.data) return <div></div>
-    if (error) return 'An error has occurred: ' + error.message
 
+    if (!session || !session.user) return <div></div>
     return <div className="flex flex-col gap-6">
-        {data.map((p) => <PostsCard key={p.id}
+        {posts.map((p) => <PostsCard key={p.id}
             post={p}
-            userId={Number(session.data.user.id)}
+            userId={Number(session.user.id)}
         />)}
     </div>
 
